d0f78aee9cc1a450d3406691030975dd507e8836
chazevo2 <chazevo2@gmail.com>
2019-07-26 10:26:39 +0900

    Servlet outstream add

--- 02.mms_db/src/com/work/controller/SignupServlet.java
+++ 02.mms_db/src/com/work/controller/SignupServlet.java
@@ -1,6 +1,7 @@
 package com.work.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -56,7 +57,18 @@ public class SignupServlet extends HttpServlet {
 		
 		if(isSignup) {
 			// 회원가입 성공
-			response.sendRedirect("login.jsp");
+//			response.sendRedirect("login.jsp");
+			
+			// 회원가입완료 안내 메세지를 경고창 메세지로 띄어주고 확인버튼을 클릭하면 login.jsp 페이지로 이동하도록 변경
+			// 1. 응답을 위한 mime type과 charset 설정
+			response.setContentType("text/html;charset=utf-8");
+			// 2. text/html 형식의 응답 출력을 위한 출력 스트림 생성
+			PrintWriter out = response.getWriter();
+			// 3. 출력스트림을 이용해서 사용자 브라우저에 html 형식의 
+			out.println("<script type='text/javascript'>");
+			out.println("alert('"+memberId+"님 회원가입이 완료되었습니다. 로그인후 이용바랍니다.')");
+			out.println("location.href='login.jsp'");
+			out.println("</script>");
 		} else {
 			// 회원가입 실패
 			request.setAttribute("message", "회원가입이 정상적으로 수행되지 않았습니다. 다시 확인하시기 바랍니다.");
